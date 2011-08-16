50806d44b2b442583b114d2cd12314b2ce9b523d
Mauricio Quezada <waxkun@gmail.com>
2011-08-15 23:02:23 -0400

    methods for points_controller

--- app/controllers/points_controller.php
+++ app/controllers/points_controller.php
@@ -1,5 +1,51 @@
 <?php
+/**
+ * 
+ * Checks and updates user points
+ * @author mquezada
+ *
+ */
 class PointsController extends AppController {
 	var $name = 'Points';
 	var $uses = array();
+	
+	/**
+	 * 
+	 * redirects user to challenges
+	 */
+	function earn() {
+		
+	}
+	
+	/**
+	 * 
+	 * checks if user has enough points
+	 * if he does, dispatch
+	 * otherwise => challenge
+	 * 
+	 */
+	function check() {
+		
+	}
+		
+	/**
+	 * 
+	 * receives challenge result and add 
+	 * points if successful 
+	 * 
+	 */
+	function reward() {
+		
+	}
+	
+	/**
+	 * 
+	 * dispatch to desired action
+	 * upload
+	 * download
+	 * index (earn points)
+	 */
+	function dispatch() {
+		
+	}	
 }
