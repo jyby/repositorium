a8a2494b67b32e268d706d83f0fcd5bdba9ec1c0
Mauricio Quezada <waxkun@gmail.com>
2011-08-20 17:23:01 -0400

    set and view repository

--- app/controllers/repositories_controller.php
+++ app/controllers/repositories_controller.php
@@ -5,10 +5,28 @@ class RepositoriesController extends AppController {
 	
 	function beforeFilter() { }
 	
-	function index() {
-		if($this->getConnectedUser() == $this->anonymous)
-			$this->redirect(array('controller' => 'login'));
-		$this->redirect('create');
+	function index($repo_url) {
+		if(isset(Configure::read('Repository.current'))) {
+			$data = $this->Repository->find('first', array('conditions' => array(
+				'Repository.url' => Configure::read('Repository.current')
+				)				
+			));			
+			$this->set(compact('data'));
+		} elseif(!is_null($repo_url)) {
+			$data = $this->Repository->find('first', array('conditions' => array(
+				'Repository.url' => $repo_url
+				)
+			));
+			
+			if(!is_null($data) && !empty($data)) {
+				Configure::write('Repository.current', $data['Repository']['url']);
+				$this->set(compact('data'));
+			} else {
+				$this->e404();
+			}
+		} else {
+			$this->e404();
+		}
 	}
 	
 	function create() {
@@ -22,37 +40,32 @@ class RepositoriesController extends AppController {
 			if($this->Repository->validates()) {
 				$repository = $this->Repository->createNewRepository($this->data, $user);
 				
-				$this->redirect(array('controller' => 'repositories', 'action' => 'view', $repository['Repository']['name']));
-						
+				$this->_set_repository($repo_data = array('repo_id' => $repository['Repository']['id']));				
 			} else {
 				$this->Session->setFlash($this->Repository->invalidFields(), 'flash_errors');
 			}	
 		}		
 	}
 	
-	function _set_repository($id) {
-		if(!is_null($id) && gettype($id) == 'integer')
-			$this->Session->write('Repository.id', $id);		
-	}
-	
-	/**
-	 * 
-	 * Enter description here ...
-	 * @TODO this method
-	 */
-	function set_repository($name) {
-		if(!is_null($name) && gettype($name) == 'string')
-			;
-	}
-	
-	function view($repository) {
-		if(is_null($repository))
+	function _set_repository($repo_data = array()) {
+		if(is_null($repo_data) || empty($repo_data))
 			$this->e404();
-		$repo = $this->Repository->findByName($repository);
 		
-		if(empty($repo))
-			$this->e404();
-
-		$this->_set_repository($repo['Repository']['id']);
+		if(isset($repo_data['repo_id'])) {
+			$c = array(
+				'Repository.id' => $repo_data['repo_id']
+			);
+		} else {
+			$c = array(
+				'Repository.name' => $repo_data['repo_name']
+			);
+		}
+		
+		$repo = $this->Repository->find('first', array('conditions' => $c));
+		
+		if(!is_null($repo) && !empty($repo)) {
+			Configure::write('Repository.current', $repo['Repository']['name']);
+		}
+		$this->redirect(array('controller' => 'repositories', 'action' => 'view', $repo['Repository']['name']));
 	}
 }
