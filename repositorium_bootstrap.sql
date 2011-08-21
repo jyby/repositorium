782867fdada703b49e9f8b50af102cf01c46dcfd
Mauricio Quezada <waxkun@gmail.com>
2011-08-21 18:10:24 -0300

    same as repositorium.sql. TODO: clean files

---
+++ repositorium_bootstrap.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.3.2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 21-08-2011 a las 16:24:20
+-- Versión del servidor: 5.5.15
+-- Versión de PHP: 5.2.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `repositorium`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `criterias`
+--
+
+CREATE TABLE IF NOT EXISTS `criterias` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `repository_id` int(255) NOT NULL,
+  `question` text NOT NULL,
+  `answer_1` varchar(255) NOT NULL,
+  `answer_2` varchar(255) NOT NULL,
+  `documentpack_size` int(11) NOT NULL,
+  `documentpack_cost` int(11) NOT NULL,
+  `documentupload_cost` int(11) NOT NULL,
+  `documentvalidation_reward` int(11) NOT NULL,
+  `penalization_a` double NOT NULL,
+  `penalization_b` double NOT NULL,
+  `depenalization_a` double NOT NULL,
+  `depenalization_b` double NOT NULL,
+  `minchallenge_size` int(11) NOT NULL,
+  `maxchallenge_size` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `criterias_documents`
+--
+
+CREATE TABLE IF NOT EXISTS `criterias_documents` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `document_id` int(255) NOT NULL,
+  `criteria_id` int(255) NOT NULL,
+  `official_answer` tinyint(1) DEFAULT NULL,
+  `total_answers_1` int(255) NOT NULL,
+  `total_answers_2` int(255) NOT NULL,
+  `validated` tinyint(1) NOT NULL,
+  `challengeable` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `criterias_users`
+--
+
+CREATE TABLE IF NOT EXISTS `criterias_users` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) NOT NULL,
+  `criteria_id` int(255) NOT NULL,
+  `challenge_size` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `documents`
+--
+
+CREATE TABLE IF NOT EXISTS `documents` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `title` varchar(512) NOT NULL,
+  `content` text NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `repository_id` int(255) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `experts`
+--
+
+CREATE TABLE IF NOT EXISTS `experts` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) NOT NULL,
+  `repository_id` int(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `repositories`
+--
+
+CREATE TABLE IF NOT EXISTS `repositories` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `url` varchar(32) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `description` text NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `min_points` int(11) NOT NULL DEFAULT '10',
+  `download_cost` int(11) NOT NULL DEFAULT '10',
+  `upload_cost` int(11) NOT NULL DEFAULT '10',
+  `documentpack_size` int(255) NOT NULL,
+  `challenge_reward` int(255) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `repositories_users`
+--
+
+CREATE TABLE IF NOT EXISTS `repositories_users` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `repository_id` int(255) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `points` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `document_id` int(255) NOT NULL,
+  `tag` varchar(20) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `email` varchar(128) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(128) NOT NULL,
+  `is_administrator` tinyint(1) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `first_name`, `last_name`, `password`, `salt`, `is_administrator`, `created`, `modified`, `active`) VALUES
+(1, 'annonymous', 'annonymous', 'annonymous', '', '', 0, '2011-06-23 15:54:33', '2011-06-23 15:54:33', 0),
+(2, 'admin@example.com', 'admin', 'user', 'fbe82ab72970b9940724512227185348eac9d7fd', '1738993739', 1, '2011-06-23 15:55:17', '2011-06-23 15:55:17', 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
