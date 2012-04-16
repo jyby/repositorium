365b9ab9af46aa8a1e42b7b0bfcd19cc3e0114af
Carlos Gajardo <cgajardo@litmon.com>
2012-04-16 15:46:59 -0400

    merged? with ccampos

--- app/models/new  2.php
+++
@@ -1,3 +0,0 @@
-<form id="sadasdasd" method="post" action="/admin_documentos/edit/159/2/1" accept-charset="utf-8">	
-<button type="submit" id="blabla" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only ui-state-hover" role="button">
-<span class="ui-button-text"><span class="ui-button-text">DeleteBLABLA</span></span></button></form>
